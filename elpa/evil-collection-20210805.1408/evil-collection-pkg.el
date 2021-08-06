02f6c10af2344d149296f58b2b8090d8042cbb35
Diego Caples <diegocaples@gmail.com>
2021-08-06 00:02:56 -0400

    added eshell

---
+++ elpa/evil-collection-20210805.1408/evil-collection-pkg.el
@@ -0,0 +1,14 @@
+(define-package "evil-collection" "20210805.1408" "A set of keybindings for Evil mode"
+  '((emacs "25.1")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "2c6e47e00910f6e14cbb3d1ed45f9599d08aafbb" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
